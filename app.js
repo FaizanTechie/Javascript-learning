3640247195d222091980d0aefccd2eb2e94406f0
M Faizan ul Haq <faizanulhaq391@gmail.com>
2026-07-16 23:42:04 +0500

    Practiced

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-const parent = document.querySelector("#parent");
-const btn = document.querySelector("#btn");
+// const parent = document.querySelector("#parent");
+// const btn = document.querySelector("#btn");
 
-btn.addEventListener("click", function (event) {
-    console.log("Button clicked");
-    event.stopPropagation();
-});
+// btn.addEventListener("click", function (event) {
+//     console.log("Button clicked");
+//     event.stopPropagation();
+// });
 
-parent.addEventListener("click", () => {
-    console.log("Parent clicked");
-}); 
+// parent.addEventListener("click", () => {
+//     console.log("Parent clicked");
+// }); 
+
+// const fruits = document.querySelector("#fruits");
+
+// fruits.addEventListener("click", function () {
+//     console.log(event.target.innerText);
+// })
+
+function one () {
+    return 1;
+}
+function two () {
+    return one() + one();
+}
+function three (){
+    let ans = two() + one();
+    console.log(ans);
+}
+
+three();
